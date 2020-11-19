bca4e7382888446616e37dd71f700e2557e228e2
Ubuntu <ubuntu@ip-172-31-15-206.ec2.internal>
2020-11-19 21:40:56 +0000

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+* This program simply outputs "Hello, World!" to the console.
+*
+* @author  Jacob Bonner
+* @version 1.0
+* @since   2020-11-19
+*/
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
